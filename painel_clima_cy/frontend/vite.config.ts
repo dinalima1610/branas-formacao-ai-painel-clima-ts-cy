af6f4dd2f38011c5a420831445bff92ae6c090b5
Diná Lima <dinalima.dal@gmail.com>
2026-08-11 23:12:14 -0300

    feat: deploy dos frontends para GitHub Pages

--- painel_clima_cy/frontend/vite.config.ts
+++ painel_clima_cy/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/branas-formacao-ai-painel-clima-ts-cy/painel_clima_cy/',
   plugins: [react()],
   resolve: {
     alias: {
